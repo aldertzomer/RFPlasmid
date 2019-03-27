9aa24756c0c72fb72b99e4ee09ba4b91cc7dcf34
Aldert Zomer <aldertzomer@gmail.com>
2019-03-27 11:32:44 +0100

    Create getdb.sh
    script to download and index databases

---
+++ getdb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "This script needs wget and diamond"
+echo "Downloading databases from http://klif.uu.nl/download/plasmid_db/"
+echo "Please be patient. This is a large download"
+echo
+echo "Download plasmid genes databases"
+wget http://klif.uu.nl/download/plasmid_db/plasmiddb_cge.faa
+wget http://klif.uu.nl/download/plasmid_db/plasmiddb_total.faa
+echo "Indexing files"
+diamond makedb --in plasmiddb_cge.faa -d plasmiddb_cge
+diamond makedb --in plasmiddb_total.faa -d plasmiddb_total
+echo "Done"
+echo ""
+exit 1
+
