36fa64d006609ddadbd1c2b81c4116c77a603eb8
Aldert Zomer <aldertzomer@gmail.com>
2023-06-05 13:52:50 +0200

    Update getdb.sh

--- getdb.sh
+++ getdb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "This script needs wget and diamond"
-echo "Downloading databases from http://klif.uu.nl/download/plasmid_db/"
+echo "Downloading databases from https://klif.uu.nl/download/plasmid_db/"
 echo "Please be patient. This is a large download"
 echo
 echo "Download plasmid genes databases"
-wget -O plasmiddb_cge.faa http://klif.uu.nl/download/plasmid_db/plasmiddb_cge.faa
-wget -O plasmiddb_total.faa http://klif.uu.nl/download/plasmid_db/plasmiddb_total.faa
+wget -O plasmiddb_cge.faa https://klif.uu.nl/download/plasmid_db/plasmiddb_cge.faa
+wget -O plasmiddb_total.faa https://klif.uu.nl/download/plasmid_db/plasmiddb_total.faa
 echo "Indexing files"
 diamond makedb --in plasmiddb_cge.faa -d plasmiddb_cge
 diamond makedb --in plasmiddb_total.faa -d plasmiddb_total
